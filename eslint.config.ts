b5736213198ffc4a28924d14136d3b4e4db82cbc
phuocht <phuocht@d-soft.com.vn>
2025-11-12 08:58:29 +0700

    feat: source

--- eslint.config.ts
+++ eslint.config.ts
@@ -29,80 +29,8 @@ export default typescriptEslint.config(
     },
     plugins: { prettier: pluginPrettier },
     rules: {
-      // Base
-      'func-style': ['error', 'expression', { allowArrowFunctions: true }],
-
       // Rule Prettier:
       'prettier/prettier': 'error',
-
-      // Vue Rules
-      'vue/no-unused-vars': 'error',
-      'vue/require-typed-ref': 'error',
-      'vue/no-v-html': 'error',
-      'vue/comma-dangle': ['error', 'never'],
-      'vue/multi-word-component-names': 'off',
-      'vue/prop-name-casing': ['error', 'camelCase'],
-      'vue/block-order': [
-        'error',
-        {
-          order: ['script', 'template', 'style'],
-        },
-      ],
-      'vue/block-lang': [
-        'error',
-        {
-          script: { lang: 'ts' },
-        },
-      ],
-      'vue/component-name-in-template-casing': [
-        'error',
-        'PascalCase',
-        {
-          registeredComponentsOnly: true,
-        },
-      ],
-      'vue/define-emits-declaration': ['error', 'type-literal'],
-      'vue/define-macros-order': [
-        'error',
-        {
-          order: [
-            'defineOptions',
-            'defineModel',
-            'defineProps',
-            'defineEmits',
-            'defineExpose',
-            'defineSlots',
-          ],
-          defineExposeLast: false,
-        },
-      ],
-      'vue/define-props-declaration': ['error', 'type-based'],
-      'vue/define-props-destructuring': [
-        'error',
-        {
-          destructure: 'never',
-        },
-      ],
-      'vue/require-macro-variable-name': [
-        'error',
-        {
-          defineProps: 'props',
-          defineEmits: 'emit',
-          defineSlots: 'slots',
-          useSlots: 'slots',
-          useAttrs: 'attrs',
-        },
-      ],
-      'vue/slot-name-casing': ['error', 'kebab-case'],
-      'vue/custom-event-name-casing': ['error', 'kebab-case'],
-      'vue/v-on-event-hyphenation': [
-        'error',
-        'always',
-        {
-          autofix: true,
-        },
-      ],
-      'no-unused-modules': 'off',
       // Import Rules
       'import/no-unresolved': 'off',
       'import/order': [
